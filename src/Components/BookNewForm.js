fa29391add22f02ada002d5d589592630227e1e0
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2023-08-20 11:28:41 -0400

    adding BookNewForm.js and Books.js

---
+++ src/Components/BookNewForm.js
@@ -0,0 +1,172 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const API = process.env.REACT_APP_API_URL;
+
+function BookNewForm(){
+    let navigate = useNavigate();
+
+    const addBook = (newBook) => {
+        axios   
+            .post(`${API}/books`, newBook)
+            .then(
+                () => {navigate(`/books`)},
+                (error) => console.error(error)
+            )
+            .catch((error) => {console.warn("Error adding new book:", error)})
+    }
+
+    const [book, setBook] = useState({
+        book: "",
+        author: "",
+        image: "",
+        genre: "",
+        release_year: 0,
+        has_read: false,
+        pages: 0,
+        favorite: false
+    });
+
+    const handleTextChange = (event) =>{
+        setBook({ ...book, [event.target.id]: event.target.value })
+    }
+
+    const handleReadChange = (event) =>{
+        setBook({ ...book, has_read: !book.has_read })
+    }
+
+    const handleFavoriteChange = (event) =>{
+        setBook({ ...book, favorite: !book.favorite })
+    }
+
+    const handleSubmit = (event) =>{
+        event.prevenDefault()
+        addBook(book)
+    }
+
+    return(
+        <div>
+            <form>
+                <div>
+                    <label>Book</label>
+                    <div>
+                        <input
+                            type="text"
+                            className=""
+                            id="book"
+                            value={book.book}
+                            placeholder="Book Title"
+                            onChange={handleTextChange}
+                            required
+                            />
+                    </div>
+                </div>
+
+                <div>
+                    <label>Author</label>
+                    <div>
+                        <input
+                            type="text"
+                            className=""
+                            id="author"
+                            value={author.book}
+                            placeholder="Author's Name"
+                            onChange={handleTextChange}
+                            required
+                             />
+                    </div>
+                </div>
+
+                <div>
+                    <label>Book Cover</label>
+                    <div>
+                        <input
+                            type="text"
+                            className=""
+                            id="book-cover"
+                            value={image.book} 
+                            placeholder="Book Cover URL"
+                            onChange={handleTextChange}
+                            />
+                    </div>
+                </div>
+
+                <div>
+                    <label>Genre</label>
+                    <div>
+                        <input
+                            type="text"
+                            className=""
+                            id="genre"
+                            value={genre.book}
+                            placeholder="Choose Book Genre"
+                            onChange={handleTextChange}
+                            />
+                    </div>
+                </div>
+
+                <div>
+                    <label>Release Year</label>
+                    <div>
+                        <input 
+                            type="number"
+                            className=""
+                            id="release_year"
+                            placeholder="Enter Release Year, if you know it"
+                            onChange={handleTextChange}
+                            value={book.release_year}
+                            />
+                    </div>
+                </div>
+
+                <div>
+                    <label>Read</label>
+                    <div>
+                        <input
+                            type="checkbox" 
+                            className=""
+                            id="has_read"
+                            onChange={handleReadChange}
+                            value={book.has_read}
+                            />
+                    </div>
+                </div>
+
+                <div>
+                    <label>Pages</label>
+                    <div>
+                        <input
+                            type="number" 
+                            className=""
+                            id=""
+                            placeholder="How many Pages?"
+                            onChange={handleTextChange}
+                            value={book.pages}
+                            />
+                    </div>
+                </div>
+
+                <div>
+                    <label>Favorite</label>
+                    <div>
+                        <input
+                            type="checkbox" 
+                            className=""
+                            id="favorite"
+                            onChange={handleFavoriteChange}
+                            value={book.favorite}
+                            />
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
+
+
+
+
+
+
+
+}
--- src/Components/Books.js
+++ src/Components/Books.js
@@ -1,21 +1,32 @@
-// import axios from "axios";
-// import { useState, useEffect } from "react";
-// import Book from "./Book.js"
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Book from "./Book.js"
 
-// const API = process.env.REACT_APP_API_URL;
+const API = process.env.REACT_APP_API_URL;
 
-// function Books() {
-//     const [books, setBooks] = useState([]);
-
-
-// useEffect(() =>{
-//     axios
-//         .get(`${API}/books`)
-//         .then((response) => {setBooks(response.data);
-//         .catch((error) => console.warn("Error fetching books:", error))    
-
-//         })
-// }, [])
+function Books() {
+    const [books, setBooks] = useState([]);
 
 
-// }
+    useEffect(() =>{
+        axios
+            .get(`${API}/books`)
+            .then((response) => {setBooks(response.data)
+            .catch((error) => console.warn("Error fetching books:", error))    
+
+            })
+    }, [])
+
+    return(
+        <div>
+            <h1>Total Books: {books.length}</h1>
+            <tbody>
+                {books.map((book)=>{
+                    return <Book key={book.id} book={book} />
+                })}
+            </tbody>
+        </div>
+    )
+}
+
+export default Books;
