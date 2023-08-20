4c8aad6c00b44f129e0b2a740f03418c70c1dab9
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2023-08-20 12:08:12 -0400

    switch from Comments to Reviews at the frontend

---
+++ src/Components/Reviews.js
@@ -0,0 +1,80 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Review from "./Review";
+import ReviewForm from "./ReviewForm";
+
+const API = process.env.REACT_APP_API_URL;
+
+function Reviews() {
+  const [reviews, setReviews] = useState(null);
+  let { id } = useParams();
+
+  const handleAdd = (newReview) => {
+    axios
+      .post(`${API}/cities/${id}/Reviews`, newReview)
+      .then((response) => {
+        setReviews([response.data, ...reviews]);
+      })
+      .catch((c) => console.warn("catch", c));
+  };
+
+  const handleDelete = (reviewId) => {
+    axios
+      .delete(`${API}/cities/${id}/reviews/${reviewId}`)
+      .then((response) => {
+        const copyReviewArray = [...reviews];
+        const indexDeletedReview = copyReviewArray.findIndex((review) => {
+          return review.id === reviewId;
+        });
+        copyReviewArray.splice(indexDeletedReview, 1);
+        setReviews(copyReviewArray);
+      })
+      .catch((c) => console.warn("catch", c));
+  };
+
+  const handleEdit = (updatedReview) => {
+    axios
+      .put(`${API}/cities/${id}/reviews/${updatedReview.id}`, updatedReview)
+      .then((response) => {
+        const copyReviewArray = [...reviews];
+        const indexUpdatedReview = copyReviewArray.findIndex((review) => {
+          return review.id === updatedReview.id;
+        });
+        copyReviewArray[indexUpdatedReview] = response.data;
+        setReviews(copyReviewArray);
+      })
+      .catch((c) => console.warn("catch", c));
+  };
+
+  useEffect(() => {
+    axios.get(`${API}/cities/${id}/reviews`).then((response) => {
+      if (Array.isArray(response.data)) {
+        setReviews(response.data);
+      } else {
+        console.warn("Response data is not an array:", response.data);
+      }
+    });
+  }, [id, API]);
+
+  return (
+    <section className="Reviews">
+        <h3>Add a New Review</h3>
+      <ReviewForm handleSubmit={handleAdd}>
+      </ReviewForm>
+
+      <h2 className="h2-Reviews">Reviews ({reviews ? reviews.length : 0})</h2>
+      {reviews && reviews.map((review) => (
+        <Review
+          key={review.id}
+          Review={review}
+          handleSubmit={handleEdit}
+          handleDelete={handleDelete}
+        />
+      ))}
+    </section>
+  );
+}
+
+export default Reviews;
+
