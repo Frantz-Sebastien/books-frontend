10f6a9f4205418214faf88048cb23c20532c265a
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2023-08-20 12:11:59 -0400

    Updating the Pages folder

---
+++ src/Pages/New.js
@@ -0,0 +1,13 @@
+import BoookNewForm from "../Components/BoookNewForm";
+
+function New() {
+  return (
+    <div className="New">
+      <h2>New</h2>
+      <BoookNewForm />
+    </div>
+  );
+}
+
+export default New;
+
